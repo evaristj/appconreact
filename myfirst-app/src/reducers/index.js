961705e983e26d34987437a8c2918e83cc4a4487
Evarist Jaume <evaristjaumet@gmail.com>
2018-09-10 13:06:32 +0200

    install react-router-redux

--- myfirst-app/src/reducers/index.js
+++ myfirst-app/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
-
+// despues de instalar el enrutador, lo importamos
+import { routerReducer } from 'react-router-redux;'
 import productList from './productListReducer';
 import activeProduct from './activeProductReducer';
 import cart from './cartReducer';
 
 const rootReducer = combineReducers({
+    routing: routerReducer,
     // como la clave es igual al valor, no es necesario ponerla
     productList,
     activeProduct,
